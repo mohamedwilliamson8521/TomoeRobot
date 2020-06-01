83dd679de2fe8f0f518ee296cbe27658d8e487d8
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-06-01 06:51:18 +0530

    Update __main__.py
    Bored

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -23,7 +23,7 @@ from haruka.modules.connection import connected
 PM_START = """Hello {}, my name is {}!
 
 I'm a group management bot with a few fun extras [;)](https://images4.alphacoders.com/963/963224.jpg)
-A fork of [Haruka Aya](https://gitlab.com/HarukaNetwork/OSS/HarukaAya)
+An [anime](https://telegra.ph/file/454c5d754062b80264055.jpg) themed fork of [Haruka Aya](https://gitlab.com/HarukaNetwork/OSS/HarukaAya)
 My repository: [Tomoe](https://github.com/Dank-del/TomoeRobot)
 
 Click the Help button below to find out more about how to use me to my full potential."""
